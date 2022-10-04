d2e78f5314d7324b7cbd269e5356272b4de765c7
Romina Benito <rominagisellebenito@hotmail.com>
2022-10-04 16:21:09 -0300

    agregué los otros gráficos de progreso

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ setInterval(() => {
     } else{
         counter+=1;
         Number.innerHTML= counter + "%";
-    }
-    
+    }    
+}, 30);
+
+let numeroCss = document.getElementById("numeroCss");
+let counter= 0;
+setInterval(() => {
+    if(counter ==70) {
+        clearInterval();
+    } else{
+        counter+=1;
+        Number.innerHTML= counter + "%";
+    }    
+}, 30);
+
+let numeroJs = document.getElementById("numerojs");
+let counter= 0;
+setInterval(() => {
+    if(counter ==70) {
+        clearInterval();
+    } else{
+        counter+=1;
+        Number.innerHTML= counter + "%";
+    }    
+}, 30);
+
+let numeroBootstrap = document.getElementById("numeroBootstrap");
+let counter= 0;
+setInterval(() => {
+    if(counter ==50) {
+        clearInterval();
+    } else{
+        counter+=1;
+        Number.innerHTML= counter + "%";
+    }    
 }, 30);
